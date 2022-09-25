9512197b0902e73fab26d710c7712700434a0fee
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-09-25 17:53:35 -0300

    sistema de quebrar swiper feito pela metade

--- breakpoint.js
+++ breakpoint.js
@@ -1,8 +1,19 @@
 const body = document.querySelector('body')
 let bodySize = body.clientWidth
 
+const swiperSlide1 = document.querySelector('#ss1')
+const swiperSlide2 = document.querySelector('#ss2')
+const swiperItem1 = document.querySelector('#si1')
+const swiperItem2 = document.querySelector('#si2')
+
+console.log(bodySize)
 function breakpoint() {
-  if (bodySize < 900) {}
+  if (bodySize < 591) {
+    swiperSlide1.outerHTML = ''
+    swiperSlide2.outerHTML = ''
+    swiperItem1.outerHTML = ''
+    swiperItem2.outerHTML = ''
+  }
 }
 
 breakpoint()
